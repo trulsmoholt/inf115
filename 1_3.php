c8b8f46032535f6f3abbd83cd021b80a96f5cc48
Truls <Truls.Moholt@student.uib.no>
2019-05-10 15:27:51 +0200

    ferdig

--- 1_3.php
+++ 1_3.php
@@ -1,11 +1,36 @@
-<!DOCTYPE HTML>  
-<html>
-<head>
-</head>
-<body>
 
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-	<input type="radio" name="gender" value="female">Norsk
-    <input type="radio" name="gender" value="male">English
-	<input type="submit" name="submit" value="Submit">
-</form>
+<?php
+if ($_SERVER["REQUEST_METHOD"]=="POST") {
+	if($_POST["language"]=="norsk"){
+		print('<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>
+  navn:<br>
+  <input type="text" name="name">
+  	<span class="error"><?php echo $nameError;?></span><br>
+  <br>
+	Alder:
+	<input type="number" name="age">
+	<span class="error"><?php echo $ageError;?></span><br>
+  	Fødselsdato:	
+	<input type="date" name="birthdate">
+	<span class="error"><?php echo $birthdateError;?></span>
+	<br>
+	<input type="submit" value="Submit">
+</form> ');
+}else{
+	print('<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>
+  name:<br>
+  <input type="text" name="name">
+  	<span class="error"><?php echo $nameError;?></span><br>
+  <br>
+	Age:
+	<input type="number" name="age">
+	<span class="error"><?php echo $ageError;?></span><br>
+  	Birthdate:	
+	<input type="date" name="birthdate">
+	<span class="error"><?php echo $birthdateError;?></span>
+	<br>
+	<input type="submit" value="Submit">
+</form> ');
+}
+}
+?>
